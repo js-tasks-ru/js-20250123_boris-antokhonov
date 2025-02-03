4ce3c095c290cc7773cb5bf79c879ff4aee7dda7
Boris Antokhonov <b.antokhonov@jet.su>
2025-02-03 17:39:17 +0300

    trim & uniq

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,24 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  let cntr = 1;
+  let result = size === undefined ? string : '';
+  
+  if (string.length > 0 && size > 0) {
+    result = string[0];
+    for (let i = 0; i < string.length - 1; i++) {
+      if (string[i] === string[i + 1]) {
+        cntr++;
+        if (cntr <= size) {
+          result += string[i + 1];
+        }
+      }
+      else {
+        cntr = 1;
+        result += string[i + 1];
+      }
+    }    
+  }
 
+  return result;
 }
