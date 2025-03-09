f0ab11510add26cc13ed25e56be00f175b209ee1
BorisAntokhonov <wuna4b@gmail.com>
2025-03-09 20:14:02 +0300

    homework 7.2 in progress

--- 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
+++ 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
@@ -1,20 +1,39 @@
 import fetchJson from './utils/fetch-json.js';
+import SortableTablev2 from '../../06-events-practice/1-sortable-table-v2/index.js';
 
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
-export default class SortableTable {
-  constructor(headersConfig, {
-    data = [],
-    sorted = {}
-  } = {}) {
+export default class SortableTable extends SortableTablev2 {
+  constructor(headerConfig, {url, isSortLocally = false, data = [], sorted = {}} = {}) {
+    super(headerConfig, {data});
+    this.isSortLocally = isSortLocally;
+    this.fetchData({url, sortCol: sorted.id, sortOrder: sorted.order, start: 0, end: 30}).
+    then((dataObj) => 
+    {this.dataProp = Object.values(dataObj);
+      this.render();}
+    );
 
   }
 
-  sortOnClient (id, order) {
-
+  async fetchData(options) {
+    const fullURL = new URL(options.url, BACKEND_URL);
+    fullURL.searchParams.append('_sort', options.sortCol);
+    fullURL.searchParams.append('_order', options.sortOrder);
+    fullURL.searchParams.append('_start', options.start);
+    fullURL.searchParams.append('_end', options.end);
+    const resp = await fetch(fullURL.toString(), {method: 'GET'});
+    return await resp.json();    
   }
 
-  sortOnServer (id, order) {
+  render() {
+    super.constructBody();
+  }
+
+  sortOnClient(col, order) {
+    super.sort(col, order);
+  }
+
+  sortOnServer (col, order) {
 
   }
 }
