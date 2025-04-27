a1a53fab7bf4bf3f6d3687a47e325d09ecfb47ce
BorisAntokhonov <wuna4b@gmail.com>
2025-04-27 20:57:09 +0300

    9.2 started

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -1,3 +1,62 @@
 export default class SortableList {
+    element = '';
+    ptrScreenX = 0;
+    ptrScreenY = 0;
+    deleteIcons = {};
+    grabIcons = {};
+    constructor({items}) {
+      this.element = document.createElement('ul');
+      this.element.className = 'sortable-list';
+      this.element.innerHTML = items.reduce((prev, curr, i) => {
+        curr.className = 'products-edit__imagelist-item sortable-list__item';  
+        return prev += curr.outerHTML;
+      }, '');
+      this.deleteIcons = this.element.querySelectorAll('[data-delete-handle=\'\']');
+      this.deleteIcons.forEach((el) => el.addEventListener('click', this.deleteElem));
+      this.grabIcons = this.element.querySelectorAll('[data-grab-handle=\'\']');
+      this.grabIcons.forEach((el) => el.addEventListener('mousedown', this.grabElem));
+      this.grabIcons.forEach((el) => el.addEventListener('mouseup', this.releaseElem));
+    }
+    deleteElem = function(e) {
+      const currElem = this.closest('li');
+      currElem.remove();
+    };
+    grabElem = function(event) {
+      const currElem = event.currentTarget.closest('li');
+      const placeHold = document.createElement('div');
+      placeHold.className = 'sortable-list__placeholder';
+      placeHold.style = 'width: 800px; height: 60px;';
+      currElem.after(placeHold);
+      currElem.classList.add('sortable-list__item_dragging');
+      currElem.style = 'width: 800px; height: 60px;';
+      this.ptrScreenX = event.screenX;
+      this.ptrScreenY = event.screenY;
+      event.currentTarget.addEventListener('pointermove', this.updatePostn);
+    }.bind(this);
+    releaseElem = function(event) {
+      const currElem = event.currentTarget.closest('li');
+      event.currentTarget.removeEventListener('pointermove', this.updatePostn);
+      currElem.style = '';
+      currElem.classList.remove('sortable-list__item_dragging');
+      this.element.querySelector('.sortable-list__placeholder').remove();
+    }.bind(this);
+    updatePostn = function(event) {
+      const currElem = event.currentTarget.closest('li');
+      currElem.style = `width: 800px; height: 60px; left: ${currElem.offsetLeft + event.screenX - this.ptrScreenX}px; top: ${currElem.offsetTop + event.screenY - this.ptrScreenY}px;`;
+      this.ptrScreenX = event.screenX;
+      this.ptrScreenY = event.screenY;
+      if (currElem.offsetTop > currElem.nextSibling?.nextSibling?.offsetTop) {
+        currElem.before(currElem.nextSibling.nextSibling);
+      }
+    }.bind(this);
+
+    destroy() {
+      this.deleteIcons.forEach((el) => el.removeEventListener('click', this.deleteElem));
+      this.grabIcons.forEach((el) => el.removeEventListener('mousedown', this.grabElem));
+      this.grabIcons.forEach((el) => el.removetListener('mouseup', this.releaseElem));
+      this.element.remove();
+    }
+
+
 
 }
